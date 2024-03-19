a3e032d5a20d85655e717a77866ba256744e7915
Alex / KATT <alex@tolahq.com>
2024-03-19 18:14:04 +0100

    fix it

--- javascript/src/rpc.spec.ts
+++ javascript/src/rpc.spec.ts
@@ -788,7 +788,7 @@ describe('RetoolRPC', () => {
 
     const result = await fn({}, context);
 
-    expectTypeOf(result).toEqualTypeOf<{ a?: number }>()
+    expectTypeOf(result.a).toEqualTypeOf<number | undefined>()
   })
 
 })
